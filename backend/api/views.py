3897013dbb22cf64113f8e575577afa46732ee19
will <william.g.neto@gmail.com>
2019-10-12 09:29:30 -0300

    filtrando ocorrências

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,31 +9,68 @@ class OcorrenciasView(APIViewMixin):
 
     def _get_ocorrencias(self, data):
         response = {
-            "ocorrencias": []
+            "aisps": [],
+            "risps": [],
+            "municipio": "",
+            "ocorrencias": [],
         }
 
+
+        aisp = data.get("aisp")
+        risp = data.get("risp")
         ano = data.get("ano")
         if not ano:
             ano = datetime.now().year - 1
 
-        ocorrencias = OcorrenciasMesData.objects.all().filter(
-            ano__gte=ano
-        )
+        if aisp:
+            ocorrencias = OcorrenciasMesData.objects.all().filter(
+                ano__gte=ano,
+                aisp=aisp
+            )
+        elif risp:
+            ocorrencias = OcorrenciasMesData.objects.all().filter(
+                ano__gte=ano,
+                risp=int(risp)
+            )
+        else:
+            ocorrencias = OcorrenciasMesData.objects.all().filter(
+                ano__gte=ano
+            )
+
         for o in ocorrencias:
             response["ocorrencias"].append(o.to_json())
-            
+
+            if not o.aisp in response["aisps"]:
+                response["aisps"].append(o.aisp)
+            if not o.risp in response["risps"]:
+                response["risps"].append(o.risp)
+            response["municipio"] = o.municipio            
         
         return response
     
     def _get_top_ocorrencias(self, data):
         response = {}
+
+        aisp = data.get("aisp")
+        risp = data.get("risp")
         ano = data.get("ano")
         if not ano:
             ano = datetime.now().year - 1
-        
-        ocorrencias = OcorrenciasMesData.objects.all().filter(
-            ano__gte=ano
-        )
+        if aisp:
+            ocorrencias = OcorrenciasMesData.objects.all().filter(
+                ano__gte=ano,
+                aisp=aisp
+            )
+        elif risp:
+            ocorrencias = OcorrenciasMesData.objects.all().filter(
+                ano__gte=ano,
+                risp=risp
+            )
+        else:
+            ocorrencias = OcorrenciasMesData.objects.all().filter(
+                ano__gte=ano
+            )
+
         indice = {}
         fields = ["apf", "cmp", "cmba", "fase", "aaapai"]
         for o in ocorrencias:
