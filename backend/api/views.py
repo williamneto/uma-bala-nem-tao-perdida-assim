fee86b0fdfa0e7549a19a168eec57f0451082d6f
will <william.g.neto@gmail.com>
2019-10-12 00:41:05 -0300

    api provendo ocorrencias por ano base

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,7 @@ from api.mixins import APIViewMixin
 from data.models import OcorrenciasMesData, PoliciaDpsAreas
 
 class OcorrenciasView(APIViewMixin):
-    get_services = ("get_ocorrencias", )
+    get_services = ("get_ocorrencias", "get_top_ocorrencias")
 
     def _get_ocorrencias(self, data):
         response = {
@@ -30,6 +30,21 @@ class OcorrenciasView(APIViewMixin):
         ano = data.get("ano")
         if not ano:
             ano = datetime.now().year - 1
-            
+        
+        ocorrencias = OcorrenciasMesData.objects.all().filter(
+            ano__gte=ano
+        )
+        indice = {}
+        fields = ["apf", "cmp", "cmba", "fase", "aaapai"]
+        for o in ocorrencias:
+            for key, value in o.ocorrencias.items():
+                if not key in fields:
+                    if len(key) > 1 and key in indice:
+                        indice[key] += int(value or 0)
+                    else:
+                        indice[key] = int(value or 0)
+        
+        response["top_ocorrencias"] = [{k: indice[k]} for k in sorted(indice, key=indice.get, reverse=True)]
+
         return response
 
